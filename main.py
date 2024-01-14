b966953574a0b370c19c239dcd5effbbb5d3151d
razsamuel <razek258@gmail.com>
2024-01-14 22:26:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 app = Flask(__name__)
 
 def read_file_content(file_name):
-    # Potentially vulnerable code
     file_path = os.path.join('/path/to/files', file_name)
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
@@ -14,7 +13,6 @@ def read_file_content(file_name):
 def api_read_file():
     try:
         user_input = request.json['file_name']
-        # Potentially vulnerable code: using user input in a filesystem operation
         file_content = read_file_content(user_input)
         return jsonify({'success': True, 'content': file_content})
     except Exception as e:
